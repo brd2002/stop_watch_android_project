338fb49c3643eeb307b304b62ddc2c3c98406c51
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-16 14:16:35 +0530

    this is my first commit

---
+++ app/src/main/java/com/example/stopwatchproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.stopwatchproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
